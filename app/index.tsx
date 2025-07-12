7fb4dbfee61c4c59fcff12d1508fd01b2a57163d
Sai <Saikittu872@gmail.com>
2025-07-12 16:04:46 +0530

    Implemented Nativewind

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+ 
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-5xl font-bold text-center text-blue-500 text-primary font-quicksand-bold">
+        Welcome to Fast Food Delivery App!
+      </Text>
+    </View>
+  );
+}
